0e4590c855a2e1e1a48fe81636ee17cb8da93b04
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-02-06 09:59:49 +0530

    forem formik complete

--- src/FormwithFormik.js
+++ src/FormwithFormik.js
@@ -4,7 +4,9 @@ import React from "react";
 function FormwithFormik() {
   const intialFormikForm = {
     name: "",
-    email: ""
+    email: "",
+    age: "",
+    password:""
   };
 
   const formik = useFormik({
@@ -14,6 +16,8 @@ function FormwithFormik() {
       console.log(values)
       console.log(values.name)
       console.log(values.email)
+      console.log(values.age)
+      console.log(values.password)
       action.resetForm()
     }
 
@@ -24,8 +28,16 @@ function FormwithFormik() {
       <form onSubmit={formik.handleSubmit}>
         <label htmlFor="name">Name :</label>
         <input type="text" placeholder="Enter your Name" name="name" value={formik.values.name} onChange={formik.handleChange}/> <br></br>
+        <br></br>
         <label htmlFor="email">Email :</label>
         <input type="email" placeholder="Enter your Name" name="email" value={formik.values.email} onChange={formik.handleChange}/> <br></br>
+
+        <br></br>
+        <label htmlFor="age">Age :</label>
+        <input type="number" placeholder="Enter your age" name="age" value={formik.values.age} onChange={formik.handleChange}/> <br></br>
+        <br></br>
+        <label htmlFor="password">PassWord : </label>
+        <input type="password" placeholder="Enter your password" name="password" value={formik.values.password} onChange={formik.handleChange}/> <br></br>
 
         <br></br>
         <div><input type="submit" value="submit" /></div>
