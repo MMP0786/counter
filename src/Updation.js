a7e167e69821448e550da45c6e676d0623d82564
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-02-09 19:16:50 +0530

    use ref in class

--- src/Updation.js
+++ src/Updation.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from "react";
 
 export default class Updation extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         name2: this.props.name
-      }
+  constructor(props) {
+    super(props);
+    this.myRef = createRef()
+
+    this.state = {
+      name2: this.props.name,
+    };
+  }
+  static getDerivedStateFromProps(props, state) {
+    if (props.name !== state.name2) {
+      return { name2: props.name };
     }
-    static getDerivedStateFromProps(props, state){
-        if(props.name !== state.name2){
-            return{ name2: props.name}
-        }
-        return null;
-    }
+    return null;
+  }
+  componentDidMount() {
+    // this.myRef.current.style = "red"
+    console.log(this.myRef)
+  }
   render() {
-    return (
-      <div>Updation name {this.state.name2}</div>
-    )
+    return <div ref={this.myref}>Updation name {this.state.name2}</div>;
   }
 }
